1496ef68f16c685cd4a937c81e3a325735821baf
VK55555 <120574192+VK55555@users.noreply.github.com>
2023-02-25 00:47:04 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,7 @@
 import streamlit
+git clone https://github.com/snowflakedb/snowflake-connector-python.git
+cd snowflake-connector-python
+
 import snowflake.connector
 import pandas
 streamlit.title('Zena\'s Amazing Athleisure Catalog')
